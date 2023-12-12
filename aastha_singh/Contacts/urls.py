f090932c53c8aa905a1e448290e84e8455688831
Aasthasingh29 <152844114+Aasthasingh29@users.noreply.github.com>
2023-12-12 10:22:18 -0800

    Add Update View

--- aastha_singh/Contacts/urls.py
+++ aastha_singh/Contacts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('create',create_contact_view, name="Create"),
     path('view',list_view,  name="ListView"),
     path('<id>',detail_view,  name="DetailView"),
-
+    path('<id>/update',update_view,  name="Update"),
+    path('<id>/delete', delete_view,  name="Delete"),
 ]
